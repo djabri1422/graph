10d4fea444574f68c0070db03737d7e0d08ca335
djabri1422 <elmezov99@gmail.com>
2018-05-13 21:40:32 +0300

    Update graph.cpp

--- tests/graph.cpp
+++ tests/graph.cpp
@@ -1,1 +1,19 @@
+#include <catch.hpp>
 
+#include "graph.hpp"
+
+TEST_CASE("init vortex")
+{
+  Graph A;
+  
+  std::vector<vector<int>> B = {{ 0, 1, 1, 0, 0, 0, 1 },
+	{ 1, 0, 1, 1, 0, 0, 0 },
+	{ 1, 1, 0, 0, 0, 0, 0 },
+	{ 0, 1, 0, 0, 1, 0, 0 },
+	{ 0, 0, 0, 1, 0, 1, 0 },
+	{ 0, 0, 0, 0, 1, 0, 1 },
+	{ 1, 0, 0, 0, 0, 1, 0 }};
+  
+  REQUIRE(A = B);
+  
+}
