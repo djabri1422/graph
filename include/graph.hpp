3390c27b85099793a7f105ae4faa9c4d723ecac6
djabri1422 <elmezov99@gmail.com>
2018-05-13 21:35:51 +0300

    Update graph.hpp

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 
@@ -15,3 +16,60 @@ public:
 	void search(int, int);
 	void printGraph();
 };
+
+Graph::Graph() noexcept : vortex(0)
+{
+
+	for (auto i = 0; i < vortex.size(); i++)
+	{
+		vortex[i] = 0;
+	}
+}
+
+void Graph::get_mass(vector _mass)
+{
+	mass = _mass;
+
+	for (unsigned int i = 0; i < mass.size(); i++)
+	{
+		for (unsigned int j = 0; j < mass.size(); j++)
+		{
+			std::cout << mass[i][j] << " "; 
+		}
+		std::cout << "\n";
+	}
+
+	auto temp = mass.size();
+	vortex.resize(temp);
+}
+
+void Graph::search(int st, int n)
+{
+	vortex[st] = 1;
+
+
+	for (int r = 0; r < n; r++)
+	{
+		if (mass[st][r] != 0 && vortex[r] == 0)
+		{
+			search(r, n);
+		}
+	}
+}
+
+
+void Graph::printGraph()
+{
+	for (unsigned int i = 0; i < mass.size(); i++)
+	{
+		std::cout << i << "\n";
+		for (unsigned int j = 0; j < mass.size(); j++)
+		{
+			if (mass[i][j] == 1 && mass[j][i] == 1)
+			{
+				std::cout << " - " << j << "  \n";
+			}
+		}
+		std::cout << "\n";
+	}
+}
